51dcdcb02f6d72b31ccaa98d5cbd5a2edffb32f8
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-07 08:12:46 +0530

    Light sensor

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -17,7 +17,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    AppCompatButton accelor, stop,proxi;
+    AppCompatButton accelor, stop, proxi, light;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +37,39 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     }
                 }
             });
+
+            proxi = findViewById(R.id.proxi);
+            proxi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Sensor proxi = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+                    if (proxi != null) {
+                        sm.registerListener(MainActivity.this, proxi, SensorManager.SENSOR_DELAY_NORMAL);
+                    }
+                }
+            });
+            light = findViewById(R.id.light);
+            light.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Sensor lightt = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+                    if (lightt != null) {
+                        sm.registerListener(MainActivity.this, lightt, SensorManager.SENSOR_DELAY_NORMAL);
+                    }
+                }
+            });
+            stop = findViewById(R.id.stop);
+            stop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sm.unregisterListener(MainActivity.this);
+                    Toast.makeText(MainActivity.this, "Sensor stopped", Toast.LENGTH_SHORT).show();
+
+                }
+            });
         } else {
             Toast.makeText(this, "No sensor found", Toast.LENGTH_SHORT).show();
         }
-        proxi=findViewById(R.id.proxi);
-        proxi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Sensor proxi= sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-                if(proxi!=null){
-                    sm.registerListener(MainActivity.this,proxi,SensorManager.SENSOR_DELAY_NORMAL);
-                }
-            }
-        });
-
-        stop=findViewById(R.id.stop);
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sm.unregisterListener(MainActivity.this);
-                Toast.makeText(MainActivity.this, "Sensor stopped", Toast.LENGTH_SHORT).show();
-
-            }
-        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -75,13 +85,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
         if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             TextView txt = findViewById(R.id.sensorsText);
-            txt.setText("Values "+event.values[0]);
-            if(event.values[0]>0){
+            txt.setText("Values " + event.values[0]);
+            if (event.values[0] > 0) {
                 Toast.makeText(this, "Object is far", Toast.LENGTH_SHORT).show();
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Object is near", Toast.LENGTH_SHORT).show();
             }
+        }
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            TextView txt = findViewById(R.id.sensorsText);
+            txt.setText("values of light sensor "+event.values[0]);
+
         }
     }
 
